3357a5ba73b5769d64433eca1e156ba811e984aa
analista5webcartorios <70077389+analista5webcartorios@users.noreply.github.com>
2024-03-04 14:36:32 -0300

    Debug

--- src/domain/usecases/whatsapp-usecase.js
+++ src/domain/usecases/whatsapp-usecase.js
@@ -63,6 +63,7 @@ module.exports = class WhatsappUseCase {
          * @var {object} oEnviaMensagem
          */
         const oEnviaMensagem = await helpers.ZenviaClient.enviarMensagem(oDados, oDadosCliente)
+        consoe.log(oEnviaMensagem)
 
         // Verifica se não houve cadastro
         if(oEnviaMensagem.statusCode != null && oEnviaMensagem.statusCode != 200){
